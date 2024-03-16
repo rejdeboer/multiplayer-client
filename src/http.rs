93b49d2de7ce34b4e0b621d86de427313cb4c092
Rick de Boer <rick.deboer@live.nl>
2024-03-16 16:25:50 +0100

    feat: http create_user function

--- src/http.rs
+++ src/http.rs
@@ -13,9 +13,22 @@ struct LoginBody {
     password: String,
 }
 
+#[derive(Serialize)]
+struct SignupBody {
+    email: String,
+    username: String,
+    password: String,
+}
+
 #[derive(Deserialize)]
 struct LoginResponse {
     pub token: String,
+}
+
+#[derive(Deserialize)]
+struct SignupResponse {
+    pub id: String,
+    pub email: String,
 }
 
 impl HttpClient {
@@ -43,5 +56,27 @@ impl HttpClient {
             .await?;
 
         Ok(login_response.token)
+    }
+
+    pub async fn create_user(
+        &self,
+        email: String,
+        username: String,
+        password: String,
+    ) -> Result<(), reqwest::Error> {
+        let url = format!("{}/token", self.base_url);
+        let request_body = SignupBody { email, username, password };
+
+        let login_response = self
+            .client
+            .post(url)
+            .json(&request_body)
+            .send()
+            .await?
+            .error_for_status()?
+            .json::<SignupResponse>()
+            .await?;
+
+        Ok(())
     }
 }
