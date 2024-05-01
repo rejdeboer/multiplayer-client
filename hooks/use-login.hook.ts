da7dc33a2a50087039b040271b420ce52dee2206
rejdeboer <rick.deboer@live.nl>
2024-05-01 19:03:25 +0200

    feat: store access token in cookie in hook

--- hooks/use-login.hook.ts
+++ hooks/use-login.hook.ts
@@ -1,6 +1,14 @@
-import { PUBLIC_CONFIG, Server } from "@/lib";
+import { PUBLIC_CONFIG, Server, setAccessToken } from "@/lib";
+import { useCallback } from "react";
 
-export function useLogin() {
+export function useLogin(): (email: string, password: string) => Promise<void> {
   const serverClient = Server(PUBLIC_CONFIG.SERVER_ENDPOINT)
-  return serverClient.authenticate
+
+  const login = useCallback(async (email: string, password: string) => {
+    return serverClient.authenticate(email, password)
+      // TODO: Use `expiresIn` value from server
+      .then(token => setAccessToken(token.jwt, 3600))
+  }, [])
+
+  return login
 }
