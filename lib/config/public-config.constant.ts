3714bd4321439fc12c2b95393cafc9b88919db83
rejdeboer <rick.deboer@live.nl>
2024-05-01 19:54:06 +0200

    fix: env vars are inlined by next as expected

--- lib/config/public-config.constant.ts
+++ lib/config/public-config.constant.ts
@@ -2,10 +2,10 @@ import { requireEnvironmentVariable } from "./require-environment-variable"
 
 export const PUBLIC_CONFIG = {
 	get SERVER_ENDPOINT(): string {
-		return requireEnvironmentVariable("NEXT_PUBLIC_SERVER_ENDPOINT")
+		return requireEnvironmentVariable("SERVER_ENDPOINT")
 	},
 
 	get WEBSOCKET_ENDPOINT(): string {
-		return requireEnvironmentVariable("NEXT_PUBLIC_WEBSOCKET_ENDPOINT")
+		return requireEnvironmentVariable("WEBSOCKET_ENDPOINT")
 	},
 }
--- lib/config/require-environment-variable.ts
+++ lib/config/require-environment-variable.ts
@@ -1,11 +1,22 @@
+type EnvironmentVariable = |
+	"SERVER_ENDPOINT" |
+	"WEBSOCKET_ENDPOINT"
+
 export function requireEnvironmentVariable(
-	property: string,
+	key: EnvironmentVariable,
 ): string {
-	const value = process.env[property];
+	const value = getValue(key)
 
 	if (value === undefined) {
-		throw new Error(`Environment variable ${property} is undefined`);
+		throw new Error(`Environment variable ${key} is undefined`);
 	}
 
 	return value;
+}
+
+function getValue(key: EnvironmentVariable): string | undefined {
+	switch (key) {
+		case "SERVER_ENDPOINT": return process.env.NEXT_PUBLIC_SERVER_ENDPOINT
+		case "WEBSOCKET_ENDPOINT": return process.env.NEXT_PUBLIC_WEBSOCKET_ENDPOINT
+	}
 }
