b0bfdc1168f3828f7d7418c6b7165b2c4b92ecf9
rejdeboer <rick.deboer@live.nl>
2024-05-08 18:00:26 +0200

    fix: remove placeholder browser cookie retrieval

--- lib/auth/get-access-token.ts
+++ lib/auth/get-access-token.ts
@@ -2,11 +2,6 @@ import { cookies } from "next/headers"
 import { TOKEN_KEY } from "./token-key.constant"
 
 export function getAccessToken(): string | undefined {
-	// If we call this from the browser
-	if (window != undefined) {
-		// TODO: Do we need the access token on the client?
-	}
-
 	const store = cookies()
 	return store.get(TOKEN_KEY)?.value
 }
