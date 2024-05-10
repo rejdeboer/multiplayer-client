bf20b1a1d18ba6c6ceb1514eda2d2e2828ebf15f
rejdeboer <rick.deboer@live.nl>
2024-05-10 17:11:22 +0200

    feat: dialog button component

--- components/ui/index.ts
+++ components/ui/index.ts
@@ -1,6 +1,8 @@
 export { Button } from "./button.component";
+export { DialogButton } from "./dialog-button.component";
 export { Input } from "./input.component";
 
 export type { ButtonProps } from "./button.component";
+export type { DialogButtonProps } from "./dialog-button.component";
 export type { InputProps } from "./input.component";
 
