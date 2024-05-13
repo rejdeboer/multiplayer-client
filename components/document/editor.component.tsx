06c671ccd87f6417c01af844b751e87d3b6c5848
rejdeboer <rick.deboer@live.nl>
2024-05-13 18:10:50 +0200

    feat: install yjs ecosystem

--- components/document/editor.component.tsx
+++ components/document/editor.component.tsx
@@ -3,5 +3,11 @@
 import { Editor as MonacoEditor } from "@monaco-editor/react"
 
 export function Editor() {
-	return (<MonacoEditor height="100vh" defaultLanguage="markdown" theme="vs-dark" />)
+	return (
+		<MonacoEditor
+			height="100vh"
+			defaultLanguage="markdown"
+			theme="vs-dark"
+		/>
+	)
 }
