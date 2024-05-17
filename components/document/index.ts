0607cad3ad3a8fe7a7177d5618a924162ec8641b
rejdeboer <rick.deboer@live.nl>
2024-05-17 23:01:46 +0200

    fix: remove export of removed type

--- components/document/index.ts
+++ components/document/index.ts
@@ -1,5 +1,3 @@
 export { CreateDocument } from "./create-document.component"
 export { DocumentList } from "./document-list.component"
 export { Editor } from "./editor.component"
-
-export type { DocumentListProps } from "./document-list.component"
