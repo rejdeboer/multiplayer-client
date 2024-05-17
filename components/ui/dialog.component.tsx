3d5b7a1b67f4465ce1af0e25f6534f28cd8be693
rejdeboer <rick.deboer@live.nl>
2024-05-17 14:41:26 +0200

    feat: optional className prop for dialog

--- components/ui/dialog.component.tsx
+++ components/ui/dialog.component.tsx
@@ -1,23 +1,30 @@
 import { Dialog as HeadlessDialog, DialogPanel, DialogTitle } from '@headlessui/react'
+import clsx from 'clsx'
 import { type Dispatch, type SetStateAction } from 'react'
 
 export type DialogProps = {
 	title: string,
 	isOpen: boolean,
-	setIsOpen: Dispatch<SetStateAction<boolean>>
-	children: React.ReactNode
+	setIsOpen: Dispatch<SetStateAction<boolean>>,
+	children: React.ReactNode,
+	className?: string,
 }
+
+const defaultClasses = clsx(
+	"max-w-lg space-y-4 border border-gray-700 rounded bg-gray-800 p-12"
+)
 
 export function Dialog({
 	title,
 	isOpen,
 	setIsOpen,
 	children,
+	className,
 }: DialogProps) {
 	return (
 		<HeadlessDialog open={isOpen} onClose={() => setIsOpen(false)} className="relative z-50">
 			<div className="fixed inset-0 flex w-screen items-center justify-center p-4">
-				<DialogPanel className="max-w-lg space-y-4 border border-gray-700 rounded bg-gray-800 p-12">
+				<DialogPanel className={clsx(defaultClasses, className)} >
 					<DialogTitle className="font-bold">{title}</DialogTitle>
 					{children}
 				</DialogPanel>
